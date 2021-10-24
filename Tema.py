89c56bd9ed8291ffacec458929c142c08ab7ac84
Delia Silistru <deliasilistru1@gmail.com>
2021-10-24 21:36:34 +0300

    Add Audio recording functionality, multithreading and code refactor

--- Tema.py
+++ Tema.py
@@ -1,71 +1,191 @@
-from selenium import webdriver
 import cv2
+import threading
+import time
+import subprocess
+import os
 import numpy as np
 import pyautogui
-import time
-
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pyaudio
+import wave
+import ffmpeg
 
-######### INREGISTRAREA ECRANULUI ##########
 
-# display screen resolution, get it from your OS settings
-SCREEN_SIZE = (1920, 1080)
-# define the codec
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-# create the video write object
-out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
+class VideoRecorder():
+    def __init__(self):
+        self.open = True
+        self.fps = 100
+        self.screen_size = (1920,1080)
+        self.fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        self.out = cv2.VideoWriter("temp_video.avi", self.fourcc, 16.5, (self.screen_size))
+        self.start_time = time.time()
+        self.seconds = 120
 
-start_time = time.time()
-seconds = 30
-prev = 0
-fps = 60
+    def record(self):
+        timer_start = time.time();
+        while(self.open == True):
+            # make a screenshot
+            img = pyautogui.screenshot()
+            # convert these pixels to a proper numpy array to work with OpenCV
+            frame = np.array(img)
+            # convert colors from BGR to RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # write the frame
+            self.out.write(frame)
 
-while True:
-    current_time = time.time()
-    elapsed_time = current_time - start_time
-    # make a screenshot
-    img = pyautogui.screenshot()
-    # convert these pixels to a proper numpy array to work with OpenCV
-    frame = np.array(img)
-    # convert colors from BGR to RGB
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # write the frame
-    out.write(frame)
-    if elapsed_time > seconds:
-        break
-    # show the frame
-    # cv2.imshow("screenshot", frame)
 
-# while True:
-#     elapsed_time = time.time() - prev
-#     img = pyautogui.screenshot()
+    def stop(self):
+        if self.open == True:
+            self.open = False
+            self.out.release()
+            cv2.destroyAllWindows()
+        else:
+            pass
+
+    def start(self):
+        video_tread = threading.Thread(target=self.record)
+        video_tread.start()
+
+
+class YoutubeNav():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def navigation(self):
+        self.driver.get('https://www.youtube.com/')
+        self.driver.maximize_window()
+        # Apasa butonul I AGREE
+        element = self.driver.find_element(By.LINK_TEXT, "I AGREE")
+        element.click()
+
+        # Face click pe primul video recomandat
+        first_video = self.driver.find_element(By.CSS_SELECTOR, "#dismissible > ytd-thumbnail")
+        first_video.click()
+
+        #time.sleep(120)
+
+    def start(self):
+        yt_thead = threading.Thread(target= self.navigation())
+        yt_thead.start()
+
+    def stop(self):
+        self.driver.close()
+
+class AudioRecorder():
+    def __init__(self):
+        self.open = True
+        self.rate = 44100
+        self.chunk = 1024
+        self.format = pyaudio.paInt16
+        self.channels = 2
+        self.audio_filename = "temp_audio.wav"
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(format= self.format,
+                                      channels = self.channels,
+                                      rate = self.rate,
+                                      input = True,
+                                      output = True,
+                                      frames_per_buffer = self.chunk)
+        self.frames = []
+
+    def record(self):
+        self.stream.start_stream()
+        while(self.open == True):
+            data = self.stream.read(self.chunk)
+            self.frames.append(data)
+            if self.open == False:
+                break
+
+    def stop(self):
+        if self.open == True:
+            self.open = False
+            self.stream.stop_stream()
+            self.stream.close()
+            self.audio.terminate()
+
+            waveFile = wave.open(self.audio_filename, "wb")
+            waveFile.setnchannels(self.channels)
+            waveFile.setsampwidth(self.audio.get_sample_size(self.format))
+            waveFile.setframerate(self.rate)
+            waveFile.writeframes(b"".join(self.frames))
+            waveFile.close()
+
+        pass
+
+    def start(self):
+        audio_thread = threading.Thread(target = self.record)
+        audio_thread.start()
+
+
+
+
+
+def start_YTVRecording(filename):
+    global video_thread
+    global yt_thread
+    global audio_thread
+
+    video_thread = VideoRecorder()
+    audio_thread = AudioRecorder()
+    yt_thread = YoutubeNav()
+
+    video_thread.start()
+    audio_thread.start()
+    yt_thread.start()
+
+    return filename
+
+
+# def start_video_recording(filename):
+#     global video_thread
 #
-#     if elapsed_time > 1.0/fps:
-#         prev = time.time()
-#         frame = np.array(img)
-#         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         out.write(frame)
+#     video_thread = VideoRecorder()
+#     video_thread.start()
 #
-#     cv2.waitKey(100)
-#     if prev - start_time > seconds:
-#         break
+#     return filename
+
+
+def stop_YTRecording(filename):
+    video_thread.stop()
+    audio_thread.stop()
+    yt_thread.stop()
+
+    # Makes sure the threads have finished
+    while threading.active_count() > 1:
+        time.sleep(1)
+
+    cmd = "ffmpeg -ac 2 -channel_layout stereo -i temp_audio.wav -i temp_video.avi -pix_fmt yuv420p " + filename + ".avi"
+    subprocess.call(cmd, shell=True)
 
 
 
-########## Deschide Youtube ############
-driver = webdriver.Chrome()
-driver.get('https://www.youtube.com/')
-driver.maximize_window()
 
-# Apasa butonul I AGREE
-element = driver.find_element(By.LINK_TEXT ,"I AGREE")
-element.click()
+# Required and wanted processing of final files
+def file_manager(filename):
+    local_path = os.getcwd()
 
-# Face click pe primul video recomandat
-first_video = driver.find_element(By.CSS_SELECTOR, "#dismissible > ytd-thumbnail")
-first_video.click()
+    if os.path.exists(str(local_path) + "/temp_audio.wav"):
+        os.remove(str(local_path) + "/temp_audio.wav")
+
+    if os.path.exists(str(local_path) + "/temp_video.avi"):
+        os.remove(str(local_path) + "/temp_video.avi")
+
+    if os.path.exists(str(local_path) + "/temp_video2.avi"):
+        os.remove(str(local_path) + "/temp_video2.avi")
+
+    if os.path.exists(str(local_path) + "/" + filename + ".avi"):
+        os.remove(str(local_path) + "/" + filename + ".avi")
 
 
-# make sure everything is closed when exited
-cv2.destroyAllWindows()
-out.release()
+if __name__ == "__main__":
+    filename = "Default_user"
+    file_manager(filename)
+
+    start_YTVRecording(filename)
+
+    time.sleep(120)
+
+    stop_YTRecording(filename)
+
+    print("Done")
